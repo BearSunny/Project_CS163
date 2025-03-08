c616f5fdd6aabd2c67a2526b934a1fbdb8305fa4
S-y-r-u <lechihungdo@gmail.com>
2025-03-08 09:17:56 +0700

    Update declare.h

--- declare.h
+++ declare.h
@@ -5,7 +5,9 @@
 
 typedef enum GameScreen {MAINMENU, LINKEDLIST, HASH, TREE, GRAPH} GameScreen;
 
-const int screenWidth = 800;
+const int screenWidth = 1000;
 const int screenHeight = 800;
+
+bool InsertInput = false;
 
 #endif
