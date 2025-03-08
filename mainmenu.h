63518e0417f6e38fa3f8b0eb471f9d56dc37f551
S-y-r-u <lechihungdo@gmail.com>
2025-03-08 09:18:19 +0700

    Update mainmenu.h

--- mainmenu.h
+++ mainmenu.h
@@ -4,17 +4,17 @@
 #include "declare.h"
 
 void DisplayMainMenu() {
-    DrawRectangle(0, 0, 400, 400, LIGHTGRAY);
-    DrawText("LINKED LIST", 200 - MeasureText("LINKED LIST", 40) / 2, 180, 40, DARKBROWN);
+    DrawRectangle(0, 0, 500, 400, LIGHTGRAY);
+    DrawText("LINKED LIST", 250 - MeasureText("LINKED LIST", 40) / 2, 240, 40, DARKBROWN);
 
-    DrawRectangle(400, 0, 400, 400, GRAY);
-    DrawText("HASH", 600 - MeasureText("HASH", 40) / 2, 180, 40, BROWN);
+    DrawRectangle(500, 0, 500, 400, GRAY);
+    DrawText("HASH", 750 - MeasureText("HASH", 40) / 2, 180, 40, BROWN);
 
-    DrawRectangle(0, 400, 400, 400, DARKGRAY);
-    DrawText("TREE", 200 - MeasureText("TREE", 40) / 2, 580, 40, BEIGE);
+    DrawRectangle(0, 400, 500, 400, DARKGRAY);
+    DrawText("TREE", 250 - MeasureText("TREE", 40) / 2, 580, 40, BEIGE);
 
-    DrawRectangle(400, 400, 400, 400, YELLOW);
-    DrawText("GRAPH", 600 - MeasureText("GRAPH", 40) / 2, 580, 40, DARKPURPLE);
+    DrawRectangle(500, 400, 500, 400, YELLOW);
+    DrawText("GRAPH", 750 - MeasureText("GRAPH", 40) / 2, 580, 40, DARKPURPLE);
 }
 
 #endif
