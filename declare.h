999fe89684449b44656346908b7315f90b6818ec
S-y-r-u <lechihungdo@gmail.com>
2025-03-09 11:33:22 +0700

    Update declare.h

--- declare.h
+++ declare.h
@@ -2,12 +2,35 @@
 #define DECLARE_H
 
 #include <raylib.h>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 typedef enum GameScreen {MAINMENU, LINKEDLIST, HASH, TREE, GRAPH} GameScreen;
 
 const int screenWidth = 1000;
 const int screenHeight = 800;
 
-bool InsertInput = false;
+bool InsertButton = false;
+bool DeleteButton = false;
+bool FindButton = false;
+int framecount = 0;
+char number[10] = {};
+int length = 0;
+
+void Insertnumber(int x) {
+    if(length == 4)
+        return;
+    number[length] = x + '0';
+    ++length;
+    number[length] = 0;
+}
+
+void Deletenumber() {
+    if(length == 0)
+        return;
+    --length;
+    number[length] = 0;
+}
 
 #endif
