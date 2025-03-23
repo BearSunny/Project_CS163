a7ef56800a47f0afc9d520ff0fbd0fc28e5bdef6
S-y-r-u <lechihungdo@gmail.com>
2025-03-23 19:17:46 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,61 +1,17 @@
-#include "mainmenu.h"
-#include "LinkedListVisualizer0.h"
-#include "LinkedList0.h"
-#include "hashvisual.h"
-#include "treevisual.h"
-#include "graphvisual.h"
-
-bool MouseButtonPressed(float x, float y, float u, float v) {
-    Vector2 p = GetMousePosition();
-    if(p.x >= x && p.x < u && p.y >= y && p.y < v)
-        return true;
-    return false;
-}
-
-void Updatenumber() {
-    if(IsKeyPressed(KEY_ZERO))
-        Insertnumber(0);
-    else if(IsKeyPressed(KEY_ONE))
-        Insertnumber(1);
-    else if(IsKeyPressed(KEY_TWO))
-        Insertnumber(2);
-    else if(IsKeyPressed(KEY_THREE))
-        Insertnumber(3);
-    else if(IsKeyPressed(KEY_FOUR))
-        Insertnumber(4);
-    else if(IsKeyPressed(KEY_FIVE))
-        Insertnumber(5);
-    else if(IsKeyPressed(KEY_SIX))
-        Insertnumber(6);
-    else if(IsKeyPressed(KEY_SEVEN))
-        Insertnumber(7);
-    else if(IsKeyPressed(KEY_EIGHT))
-        Insertnumber(8);
-    else if(IsKeyPressed(KEY_NINE))
-        Insertnumber(9);
-    else if(IsKeyPressed(KEY_BACKSPACE))
-        Deletenumber();
-}
+#include "source/mainmenu.cpp"
+#include "header/LinkedListVisualizer0.h"
+#include "header/LinkedList0.h"
+#include "header/hashvisual.h"
+#include "source/treevisual.cpp"
+#include "header/graphvisual.h"
 
 int main () {
     InitWindow(screenWidth, screenHeight, "DSA VISUALIZATION");
-    GameScreen currentScreen = MAINMENU;
     SetTargetFPS(60);
     while(!WindowShouldClose()) {
         switch(currentScreen) {
             case MAINMENU:
-                if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-                    if(MouseButtonPressed(0, 0, 500, 400)) {
-                        currentScreen = LINKEDLIST;
-                        InitLinkedList();
-                    }
-                    else if(MouseButtonPressed(500, 0, 1000, 400))
-                        currentScreen = HASH;
-                    else if(MouseButtonPressed(0, 400, 500, 800))
-                        currentScreen = TREE;
-                    else if(MouseButtonPressed(500, 400, 1000, 800))
-                        currentScreen = GRAPH;
-                }
+                MAINMENU_INTERACT();
                 break;
             case LINKEDLIST:
                 if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
@@ -72,42 +28,7 @@ int main () {
                 }
                 break;
             case TREE:
-                if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-                    if(MouseButtonPressed(200, 10, 300, 40)) {
-                        if(!InsertButton) {
-                            InsertButton = true;
-                            length = 0;
-                            number[0] = 0;
-                            framecount = 0;
-                        }
-                    }
-                    else
-                        InsertButton = false;
-                    if(MouseButtonPressed(305, 10, 405, 40))
-                        InsertAVL();
-                    if(MouseButtonPressed(410, 10, 510, 40)) {
-                        if(!DeleteButton) {
-                            DeleteButton = true;
-                            length = 0;
-                            number[0] = 0;
-                            framecount = 0;
-                        }
-                    }
-                    else
-                        DeleteButton = false;
-                    if(MouseButtonPressed(620, 10, 720, 40)) {
-                        if(!FindButton) {
-                            FindButton = true;
-                            length = 0;
-                            number[0] = 0;
-                            framecount = 0;
-                        }
-                    }
-                    else
-                        FindButton = false;
-                }
-                else if(InsertButton || DeleteButton || FindButton)
-                    Updatenumber();
+                TREE_INTERACT();
                 break;
             case GRAPH:
                 if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
