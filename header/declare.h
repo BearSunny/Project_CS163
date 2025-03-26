a238267fe4c774924587dd1230051634e8597588
mbmb <phamminhduc2011@gmail.com>
2025-03-27 01:29:43 +0700

    add declare.cpp + texture resource

--- header/declare.h
+++ header/declare.h
@@ -6,91 +6,50 @@
 
 using namespace std;
 
-typedef enum GameScreen {MAINMENU, LINKEDLIST, HASH, TREE, GRAPH} GameScreen;
+// Enums and structs
+typedef enum GameScreen { MAINMENU, LINKEDLIST, HASH, TREE, GRAPH } GameScreen;
 
-inline Font FONT = LoadFontEx("./Font/SF-Pro-Display-Regular.otf", 40, 0, 255);
+struct theme {
+    Color BACKGROUND;
+    Color MENU_BUTTON;
+    Color HOVER_MENU;
+    Color BUTTON;
+    Color HOVER_BUTTON;
+    Color SEPERATOR;
+    Color INFOR;
+    Color HIGHLIGHT_TEXT;
+    Color HIGHLIGHT_NODE_1;
+    Color HIGHLIGHT_NODE_2;
+    Color NODE;
+    Color LINE;
+    Color SIDEBAR;
+    Color WEIGHT;
+    bool isDarkMode = true;
+};
 
-const int screenWidth = 1000;
-const int screenHeight = 800;
+// Global variable declarations
+extern theme THEME;
+extern Font FONT;
+extern Texture2D PLAY, PAUSE, REPLAY, BACK, DOUBLE_BACK, NEXT, DOUBLE_NEXT, SPEED_UP, SPEED_DOWN, BACK_PAGE;
 
-GameScreen currentScreen = MAINMENU;
+extern const int screenWidth;
+extern const int screenHeight;
+extern const Vector2 ORIGIN;
+extern const float CODE_SIZE;
 
-bool InsertButton = false;
-bool DeleteButton = false;
-bool FindButton = false;
-int FindQuerry = -1;
-int FindNumber = 0;
-int framecount = 0;
-char number[10] = {};
-int length = 0;
+extern GameScreen currentScreen;
+extern bool InsertButton, DeleteButton, FindButton;
+extern int FindQuerry, FindNumber, framecount, length;
+extern char number[10];
 
-void Insertnumber(int x) {
-    if(length == 4)
-        return;
-    number[length] = x + '0';
-    ++length;
-    number[length] = 0;
-}
-
-void Deletenumber() {
-    if(length == 0)
-        return;
-    --length;
-    number[length] = 0;
-}
-
-void DrawNumber(int data, int x, int y, int fs) {
-    int len = 0;
-    int tmp = data;
-    while(data) {
-        ++len;
-        data /= 10;
-    }
-    if(len == 0) {
-        DrawText("0", x - MeasureText("0", fs) / 2, y - fs / 2, fs, BLACK);
-        return;
-    }
-    char *c = new char[len + 1];
-    c[len] = 0;
-    while(tmp) {
-        --len;
-        c[len] = tmp % 10 + '0';
-        tmp /= 10;
-    }
-    DrawText(c, x - MeasureText(c, fs) / 2, y - fs / 2, fs, BLACK);
-    delete[] c;
-}
-
-bool MouseButtonPressed(float x, float y, float u, float v) {
-    Vector2 p = GetMousePosition();
-    if(p.x >= x && p.x < u && p.y >= y && p.y < v)
-        return true;
-    return false;
-}
-
-void Updatenumber() {
-    if(IsKeyPressed(KEY_ZERO))
-        Insertnumber(0);
-    else if(IsKeyPressed(KEY_ONE))
-        Insertnumber(1);
-    else if(IsKeyPressed(KEY_TWO))
-        Insertnumber(2);
-    else if(IsKeyPressed(KEY_THREE))
-        Insertnumber(3);
-    else if(IsKeyPressed(KEY_FOUR))
-        Insertnumber(4);
-    else if(IsKeyPressed(KEY_FIVE))
-        Insertnumber(5);
-    else if(IsKeyPressed(KEY_SIX))
-        Insertnumber(6);
-    else if(IsKeyPressed(KEY_SEVEN))
-        Insertnumber(7);
-    else if(IsKeyPressed(KEY_EIGHT))
-        Insertnumber(8);
-    else if(IsKeyPressed(KEY_NINE))
-        Insertnumber(9);
-    else if(IsKeyPressed(KEY_BACKSPACE))
-        Deletenumber();
-}
+// Function declarations
+void initResource();
+void Insertnumber(int x);
+void Deletenumber();
+void DrawNumber(int data, int x, int y, int fs);
+bool MouseButtonPressed(float x, float y, float u, float v);
+void Updatenumber();
+void drawPicture(const char* path, Rectangle desRec, float rotation, Vector2 origin, Color color);
+void drawPicture(Texture2D texture, Rectangle desRec, float rotation, Vector2 origin, Color color);
 
 #endif
---
+++ source/declare.cpp
@@ -0,0 +1,144 @@
+#include "../header/declare.h"
+
+// Global variable definitions
+Texture2D PLAY;
+Texture2D PAUSE;
+Texture2D REPLAY;
+Texture2D NEXT;
+Texture2D DOUBLE_NEXT;
+Texture2D BACK;
+Texture2D DOUBLE_BACK;
+Texture2D SPEED_UP;
+Texture2D SPEED_DOWN;
+Texture2D BACK_PAGE;
+theme THEME;
+Font FONT;
+
+const int screenWidth = 1000;
+const int screenHeight = 800;
+const Vector2 ORIGIN = {0.0f, 0.0f};
+const float CODE_SIZE = 15;
+
+GameScreen currentScreen;
+bool InsertButton, DeleteButton, FindButton;
+int FindQuerry, FindNumber, framecount, length;
+char number[10];
+
+// Function implementations
+
+void initResource() 
+{
+    GameScreen currentScreen = MAINMENU;
+    bool InsertButton = false, DeleteButton = false, FindButton = false;
+    int FindQuerry = -1, FindNumber = 0, framecount = 0, length = 0;
+    char number[10] = {};
+
+    PLAY = LoadTexture("./asset/Play.png");
+    PAUSE = LoadTexture("./asset/Pause.png");
+    REPLAY = LoadTexture("./asset/Replay.png");
+    BACK = LoadTexture("./asset/Chevron left.png");
+    DOUBLE_BACK = LoadTexture("./asset/Chevrons left.png");
+    NEXT = LoadTexture("./asset/Chevron right.png");
+    DOUBLE_NEXT = LoadTexture("./asset/Chevrons right.png");
+    SPEED_UP = LoadTexture("./asset/Chevron up.png");
+    SPEED_DOWN = LoadTexture("./asset/Chevron down.png");
+    BACK_PAGE = LoadTexture("./asset/BackPage.png");
+    
+    //Color
+    THEME.BACKGROUND = {229,247,255, 255};
+    THEME.MENU_BUTTON = {255,255,255,255};
+    THEME.HOVER_MENU = {197,252,245,255};
+    THEME.BUTTON = {229,247,255, 255};
+    THEME.HOVER_BUTTON = {125,215,255, 255};
+    THEME.SEPERATOR = {3, 178, 255, 255};
+    THEME.INFOR = {6, 155, 91, 255};
+    THEME.HIGHLIGHT_TEXT = {157, 238, 238, 255};
+    THEME.HIGHLIGHT_NODE_1 = {55,221,149,255};
+    THEME.HIGHLIGHT_NODE_2 = Fade(BLUE,60);
+    THEME.NODE = {249,208,208,255};
+    THEME.LINE = {182,157,157, 255};
+    THEME.SIDEBAR = {250, 254, 255, 255};
+    THEME.WEIGHT = {6, 155, 91, 255};
+    //FONT = LoadFont("./Font/Roboto-Regular.ttf");
+    FONT = LoadFontEx("./Font/SF-Pro-Display-Regular.otf", 40, 0, 255);
+    
+}
+
+void Insertnumber(int x) {
+    if (length == 4) return;
+    number[length] = x + '0';
+    ++length;
+    number[length] = 0;
+}
+
+void Deletenumber() {
+    if (length == 0) return;
+    --length;
+    number[length] = 0;
+}
+
+void DrawNumber(int data, int x, int y, int fs) {
+    int len = 0;
+    int tmp = data;
+    while (data) {
+        ++len;
+        data /= 10;
+    }
+    if (len == 0) {
+        DrawText("0", x - MeasureText("0", fs) / 2, y - fs / 2, fs, BLACK);
+        return;
+    }
+    char* c = new char[len + 1];
+    c[len] = 0;
+    while (tmp) {
+        --len;
+        c[len] = tmp % 10 + '0';
+        tmp /= 10;
+    }
+    DrawText(c, x - MeasureText(c, fs) / 2, y - fs / 2, fs, BLACK);
+    delete[] c;
+}
+
+bool MouseButtonPressed(float x, float y, float u, float v) {
+    Vector2 p = GetMousePosition();
+    return (p.x >= x && p.x < u && p.y >= y && p.y < v);
+}
+
+void Updatenumber() {
+    if (IsKeyPressed(KEY_ZERO))
+        Insertnumber(0);
+    else if (IsKeyPressed(KEY_ONE))
+        Insertnumber(1);
+    else if (IsKeyPressed(KEY_TWO))
+        Insertnumber(2);
+    else if (IsKeyPressed(KEY_THREE))
+        Insertnumber(3);
+    else if (IsKeyPressed(KEY_FOUR))
+        Insertnumber(4);
+    else if (IsKeyPressed(KEY_FIVE))
+        Insertnumber(5);
+    else if (IsKeyPressed(KEY_SIX))
+        Insertnumber(6);
+    else if (IsKeyPressed(KEY_SEVEN))
+        Insertnumber(7);
+    else if (IsKeyPressed(KEY_EIGHT))
+        Insertnumber(8);
+    else if (IsKeyPressed(KEY_NINE))
+        Insertnumber(9);
+    else if (IsKeyPressed(KEY_BACKSPACE))
+        Deletenumber();
+}
+
+void drawPicture(const char* path, Rectangle desRec, float rotation, Vector2 origin, Color color) {
+    Texture2D texture = LoadTexture(path);
+    if (texture.id == 0) return;
+    Rectangle srcRect = {0.0f, 0.0f, (float)texture.width, (float)texture.height};
+    DrawTexturePro(texture, srcRect, desRec, origin, rotation, color);
+    UnloadTexture(texture);
+}
+
+void drawPicture(Texture2D texture, Rectangle desRec, float rotation, Vector2 origin, Color color) {
+    if (texture.id == 0) return;
+    Rectangle srcRect = {0.0f, 0.0f, (float)texture.width, (float)texture.height};
+    DrawTexturePro(texture, srcRect, desRec, origin, rotation, color);
+}
