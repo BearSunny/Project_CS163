a9e2b3cecca19badba8bba3e6cf17e4e134d176e
mbmb <phamminhduc2011@gmail.com>
2025-04-10 16:09:24 +0700

    add variables for graph

--- header/graphvisual.h
+++ header/graphvisual.h
@@ -12,22 +12,27 @@ struct GraphNode {
     std::vector<Edge> neighbors;
     Vector2 pos;
     int color = 0;
+    int data;
 };
 
 class Graph {
     private:
         std::vector<GraphNode*> nodes;
         int frameCounting = 0;
+        GraphNode* checkValidNode[200];
         int findParent(std::vector<int>& parent, int node);
         void unionNodes(std::vector<int>& parent, std::vector<int>& rank, int u, int v);
         int getNodeIndex(GraphNode* node);
     public:
+        Graph();
+        GraphNode* getNode(int index);
     void clearGraph();
     ~Graph();
 
     void addNode(GraphNode* node);
     void addNode();
     int addEdge(int from, int to, int weight);
+    int addUserEdge(int from, int to, int weight);
     void randomize(int nodeCount, int maxX, int maxY);
     void initFromFile(const char* filename);
     void resetMark();
@@ -35,6 +40,9 @@ class Graph {
     void mstKruskal();
     void drawGraph(Font font, std::vector<Color> color={});
     void updatePositions();
+    void updateNodeValue(int nodeNum, int n_val);
+    void updateCheckArray(int change, int old, GraphNode* node);
+    void deleteNode(int nodeNumber);
 };
 
 class GraphVisualize {
@@ -52,9 +60,25 @@ class GraphVisualize {
         Button randomButton;
         Button loadFileButton;
         bool isCreateChosen;
+        bool isUpdateChosen;
+        bool isDeleteChosen;
         
         Button connectedComponentButton;
         Button mstKruskalButton;
+
+        InputStr fromInput;
+        InputStr toInput;
+        InputStr weightInput;
+        InputStr nodeNumberInput;
+        InputStr updateValueInput;
+        Button submitButton;
+        Button addVertexButton;
+        Button updateButton;
+        Button deleteButton;
+        Button updateSubmitButton;
+        Button deleteSubmitButton;
+        Button cancelButton;
+
         
         Font font = LoadFontEx("./Font/SF-Pro-Display-Regular.otf", 40, 0, 255);
         std::vector<Color> generateRandomColors(int n);
@@ -66,6 +90,8 @@ class GraphVisualize {
         void mstKruskal();
         void drawGraph();
         void drawButton();
+        void drawForm();
+        void drawButtons();
         void draw();
         int handle();
 };
